752f62017c35c4e913dd01f2074069ead43746fa
Jacek Weremko <jacek.weremko@gmail.com>
2024-05-05 19:11:18 +0200

    Enable BrowserProfilingIntegration

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -7,6 +7,11 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://981e19c8f1d09c9dce2943934de19478@o4507203850207232.ingest.de.sentry.io/4507203851976784",
 
+  integrations: [
+    // Add browser profiling integration to the list of integrations
+    new Sentry.BrowserProfilingIntegration(),
+  ],
+
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
   profilesSampleRate: 1.0,
